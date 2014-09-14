128989c93df74ef320bc38e692584d9a8fc884f2
Boris Miček <borismicek@iMac-uzivatela-Boris.local>
2014-09-14 21:24:23 +0200

    added test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  GitTest
+//
+//  Created by Boris Miček on 14.9.2014.
+//  Copyright (c) 2014 Boris Miček. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  GitTest
+//
+//  Created by Boris Miček on 14.9.2014.
+//  Copyright (c) 2014 Boris Miček. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
